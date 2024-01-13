5bdbf26099ef4f52518c12fd6f627424ad321270
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-01-13 14:55:32 +0100

    HTTP status codes

--- src/coffes/coffes.controller.ts
+++ src/coffes/coffes.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { response } from 'express';
 
 // Defines the route of /coffees
 @Controller('coffes')
 export class CoffesController {
   // Get Method - Caries out a get method
   @Get()
-  findAll() {
+  findAll(): string {
     return 'This action returns all coffess';
   }
 
@@ -14,11 +24,34 @@ export class CoffesController {
   getFlavors() {
     return 'Returns only coffee flavors';
   }
-  
+
   //   Adding route params eg router:123
   @Get(':id')
   getOne(@Param('id') id: string) {
     // @Params allows us to grab all incoming params and use them inside the function body
     return `This returns ${id} coffee`;
   }
+
+  //   Switching to express
+  //   @Get(':id')
+  //   getOne(@Param('id') id: string, @Res() response) {
+  //     // @Params allows us to grab all incoming params and use them inside the function body
+  //     // return `This returns ${id} coffee`;
+  //     response.status(200).send(`This returns ${id} coffee`);
+  //   }
+
+  //   POST Request and Request Body
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+
+  //   //   We can specify a specific value in the request body
+  //   //   POST Request and Request Body
+
+  //   @Post()
+  //   create(@Body('name') body) {
+  //     return body;
+  //   }
 }
