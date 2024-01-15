9004ff5563187b4134004586b17a4e2da801a2e3
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-01-15 12:07:18 +0100

    Setup postgres db with docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffesModule } from './coffes/coffes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffesModule],
+  imports: [
+    CoffesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      //   Disable sycn in prod
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffes/entities/coffee.entity.ts
+++ src/coffes/entities/coffee.entity.ts
@@ -1,4 +1,9 @@
 // Model or data type
+
+import { Entity } from 'typeorm';
+
+@Entity()
+// SQL Table === 'Coffee
 export class Coffee {
   id: number;
   name: string;
