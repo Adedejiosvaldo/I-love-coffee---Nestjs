0907eb51be5ed3ba9c58acbe0006ba002568cd38
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-01-18 02:53:52 +0100

    Schema Validation for Env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { CoffesModule } from './coffes/coffes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
-import 'dotenv/config';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.required(),
+        // DATABASE_PORT: Joi.number().default(5000),
+        PORT: Joi.number().default(5000),
+      }),
+    }),
     CoffesModule,
-    MongooseModule.forRoot(`${process.env.DATABASE_URL}`),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     CoffeeRatingModule,
     DatabaseModule,
   ],
--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffesModule } from 'src/coffes/coffes.module';
 import { DatabaseModule } from 'src/database/database.module';
-import 'dotenv/config';
-console.log(process.env.DATABASE_URL);
 
 @Module({
   imports: [
     CoffesModule,
     // Using dynamic module
-    DatabaseModule.register(`${process.env.DATABASE_URL}`),
+    // DatabaseModule.register(`${process.env.DATABASE_URL}`),
   ],
   providers: [CoffeeRatingService],
 })
